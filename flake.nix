0fb02546cc427652c1b742a9fb4ef5a6a1a9c374
Axel Denis <axel.denis@epitech.eu>
2025-07-30 14:16:48 +0200

    fix: updated flake imports

--- flake.nix
+++ flake.nix
@@ -7,8 +7,10 @@
       pkgs = import nixpkgs { inherit system; };
       lib = import ./lib { inherit (pkgs) lib; };
     in {
-      nixosModules.default = { lib, ... }@args:
-        let inherit (args) config pkgs;
-        in import ./modules { inherit lib config pkgs; };
+      nixosModules = {
+        immich = import ./modules/immich.nix;
+        jellyfin = import ./modules/jellyfin.nix;
+        default = [ ./modules/immich.nix ./modules/jellyfin.nix ];
+      };
     };
 }
